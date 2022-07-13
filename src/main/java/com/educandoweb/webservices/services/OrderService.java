9e3027d1f848a4e69103f8f886fef1cdcf1fdb7e
AlanGustav0 <alangustavo946@gmail.com>
2022-07-13 08:12:16 -0300

    Order, Instant, part 2

---
+++ src/main/java/com/educandoweb/webservices/services/OrderService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.webservices.services;
+
+import com.educandoweb.webservices.entities.Order;
+import com.educandoweb.webservices.entities.User;
+import com.educandoweb.webservices.repositories.OrderRepository;
+import com.educandoweb.webservices.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll(){
+            return orderRepository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> order = orderRepository.findById(id);
+        return order.get();
+    }
+
+
+}
